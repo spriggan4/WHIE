00583e7219446d2d4428d93f698bc52873e3510a
spriggan4 <38972792+spriggan4@users.noreply.github.com>
2020-01-28 22:43:48 +0900

    Parcelable 인터페이스로 Meal 클래스를 만듬. 이후 인텐트를 통해 TwoDaysAgoMeal 액티비티에 객체 정보 전달.

--- app/src/main/java/com/example/whie/SelectYesterdayMeal.kt
+++ app/src/main/java/com/example/whie/SelectYesterdayMeal.kt
@@ -3,13 +3,24 @@ package com.example.whie
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_select_yesterday_meal.*
+import org.jetbrains.anko.startActivity
 
 class SelectYesterdayMeal : AppCompatActivity() {
+    private lateinit var meal: Meal
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_yesterday_meal)
 
         mealTimeText.text = intent.getStringExtra("mealTime").toString()
+
+        samGyubSalText.setOnClickListener {
+            meal = Meal("삼겹살", "Korea", "Meat", "Dinner", "Oily")
+
+            var bundle = Bundle()
+            bundle.putParcelable("selected_meal", meal)
+
+            startActivity<TwoDaysAgoMeal>("bundle" to bundle)
+        }
     }
 }
